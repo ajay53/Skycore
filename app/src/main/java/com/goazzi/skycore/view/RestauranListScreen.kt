d8fb70a7c4a6c0407c35bbddd2e09967a666ce1a
Ajay Singh Thakurathi <ajayst53@gmail.com>
2023-12-20 23:28:57 +0530

    project is now running with Coil

--- app/src/main/java/com/goazzi/skycore/view/RestauranListScreen.kt
+++ app/src/main/java/com/goazzi/skycore/view/RestauranListScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -21,12 +22,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.bumptech.glide.request.RequestOptions
 import com.goazzi.skycore.R
 import com.goazzi.skycore.model.Business
@@ -53,7 +60,7 @@ fun RestaurantListItem(business: Business? = null, modifier: Modifier = Modifier
 
 //            Glide.with(context).setDefaultRequestOptions(options).load(imageUrl).into(view)
 
-            /*AsyncImage(
+            AsyncImage(
                 model = ImageRequest.Builder(LocalContext.current)
                     .data("https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI")
                     .crossfade(true)
@@ -61,8 +68,11 @@ fun RestaurantListItem(business: Business? = null, modifier: Modifier = Modifier
                 placeholder = painterResource(R.drawable.ic_restaurant),
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.clip(CircleShape)
-            )*/
+                modifier = Modifier
+                    .clip(RoundedCornerShape(2.dp))
+                    .background(color = Color.Transparent)
+                    .size(50.dp)
+            )
 
             /*Image(
                 painter = painterResource(R.drawable.ic_restaurant),
@@ -138,16 +148,16 @@ fun RestaurantListItem(business: Business? = null, modifier: Modifier = Modifier
                     .background(color = colorResource(id = R.color.holo_green_dark))
             ) {
 //                Box(contentAlignment = Alignment.Center, modifier = modifier.fillMaxSize()) {
-                    Text(
-                        text = "3.5",
-                        color = colorResource(id = R.color.white),
-                        fontWeight = FontWeight.Bold,
-                        style = MaterialTheme.typography.bodyLarge,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
+                Text(
+                    text = "3.5",
+                    color = colorResource(id = R.color.white),
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.bodyLarge,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
 //                            .fillMaxSize()
-                            .align(Alignment.Center)
-                    )
+                        .align(Alignment.Center)
+                )
 //                }
 
             }
